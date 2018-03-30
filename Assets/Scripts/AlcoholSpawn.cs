d5b702d208d22472c2892a3bf278810284a32b83
Tobi <toblerus@hotmail.de>
2018-03-30 20:29:14 +0200

    Initial commit

---
+++ Assets/Scripts/AlcoholSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlcoholSpawn : MonoBehaviour {
+
+	public Transform spawnPos;
+	public GameObject spawnee;
+	public int frames;
+
+	// Update is called once per frame
+	void Update () {
+		frames++;
+		if (frames % 20 == 0) {
+			Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+		}
+	}
+}
