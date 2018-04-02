cc62e3c9f3de9ae0ec19ca0100df77e09f7e54cc
Tobi <toblerus@hotmail.de>
2018-04-02 23:39:14 +0200

    testing gyro fix #3

--- Assets/Scripts/PhoneTiltRotate.cs
+++ Assets/Scripts/PhoneTiltRotate.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class PhoneTiltRotate : MonoBehaviour {
 
-	float zRotation;
+	float xRotation;
 	void Start(){
 		Input.gyro.enabled = false;
 		Input.gyro.enabled = true;
 	}
 	void Update(){
-		zRotation += -Input.gyro.rotationRateUnbiased.z;
+		xRotation += -Input.gyro.rotationRateUnbiased.x;
 
-		transform.eulerAngles = new Vector3(0, -90, zRotation);
+		transform.eulerAngles = new Vector3(0, -90, xRotation);
 
 	}
 	
